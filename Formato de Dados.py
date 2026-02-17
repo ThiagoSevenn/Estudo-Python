fb4d5c9eca4c64d55fa08db74826231ef3d422a5
Thiago Neves <thiagonevessc@gmail.com>
2026-02-17 00:26:58 -0300

    Atualizações em formatações de dados e Inclusão de operadores aritméticos.

--- Formato de Dados.py
+++ Formato de Dados.py
@@ -1,4 +1,6 @@
 # Formatação de string
+## '\n' serve para pular uma linha.
+print('{:_^40}'.format('Formatação string'))
 
 string = 'Olá, mundo!!'
 str = 'Olá,', ' mundo!!'
@@ -13,3 +15,34 @@ print(str)
 print(str1)
 print(str2)
 print(str3)
+
+print('_'*30)
+
+nome = input("Digite seu nome: ")
+
+# Nome vai ser escrito em 20 caracteres, seria representado somente po '{:20}'
+
+## Nome vai ser escrito formatando a esquerda '<'.
+print('Seu nome é {:<20}, Prazer!!'.format(nome))
+## Nome vai ser escrito formatando centralizado '^'.
+print('Seu nome é {:^20}, Prazer!!'.format(nome))
+## Nome vai ser escrito formatando a direita '>'.
+print('Seu nome é {:>20}, Prazer!!'.format(nome))
+## Nome vai ser escrito formatando a direita '>' e nos espaços que seriam ' ', escolhi o caracter '-' para substituir.
+print('Seu nome é {:->20}, Prazer!!'.format(nome))
+
+# A função print tem por padrão uma quebra de linha, 
+# para mudar isso, utilizasse ', end=', após o '=' escolhe a string que aparecerá ao final do print.
+print('Por padrão ele pula a linha')
+print('Sem seguir esse padrão e exigindo que ele finalize com um "terminei." ', end='terminei.')
+
+print('\n')
+
+print('{:_^40}'.format('Formatação Float'))
+
+# Formatação de tipo Float
+numero = 1.333333333
+
+print(f'O número é: {numero}')
+print(f'O número é (2 casa decimais): {numero:.2f}')
+print('O número é (4 casas decimais): {:.4f}'.format(numero))
--- Operadores aritmeticos.py
+++ Operadores aritmeticos.py
@@ -1,1 +1,26 @@
-print(2**3)
+# Ordem de processamento das expressões aritméticas. DICA: É igual a ordem de precedência com expressões matemáticas.
+# 1ª ~> ()
+# 2ª ~> **
+# 3ª ~> * , / , // , %
+# 4ª ~> + , -
+
+# Soma (+)
+print('Soma de 5+2:',5 + 2)
+
+# Subtração (-)
+print('Subtração de 5-2:',5 - 2)
+
+# Multiplicação (*)
+print('Multiplição de 5*2:',5 * 2)
+
+# Divisão (/)
+print('Divisão de 5/2:',5 / 2)
+
+# Divisão Inteira (//)
+print('Divisão inteira de 5/2:',5 // 2)
+
+# Resto da divisão (%)
+print('Resto da divisão de 5/2:',5 % 2)
+
+# Potência (**)
+print('Potência de 5**2:',5 ** 2)  
