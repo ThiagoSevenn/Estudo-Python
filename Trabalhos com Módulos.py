ec08b6236a4d4f678836af583fb8250f598700e6
Thiago Neves <thiagonevessc@gmail.com>
2026-02-19 01:39:24 -0300

    Como funciona módulos no python e criação dos desafios 16 - 21

---
+++ Trabalhos com Módulos.py
@@ -0,0 +1,23 @@
+# Assim como outras linguagens de programação o python é cheio de bibliotecas 
+# que disponibilizam ao programa utilizar diversos recursos diferentes visando suprir as necessidades do programador.
+
+# Esses módulos podem ser desenvolvidos pelo próprio programador ou por terceiros, ambas necessitam da importação para ser utilizada.
+# Porém, caso precise de uma biblioteca externa, comumente será necessário instalar ela.
+
+
+## Em python existem duas formas diferentes de se importar uma biblioteca:
+## 1ª ~> Utilizando "Importe *Nome da biblioteca*", esse import é mais generalizado
+## 2ª ~> Utilizando "From *Nome da biblioteca* import *Nome da funcionalidade*", esse import é mais objetivo
+
+# Por exemplo:
+
+## 1ª forma:
+# import math
+
+# print(f'Esse é o 5! ~> {math.factorial(5)}')
+
+## 2ª forma: Essa forma é otimizada devido a requisição ser mais específica,
+#            sem guardar na memória funções que não serão utilizadas.
+from math import factorial
+
+print(f'Esse é o 5! ~> {factorial(5)}')
---
+++ desafios/Desafio 016.py
@@ -0,0 +1,1 @@
+# Crie um programa que leia um número Real qualquer e mostre na tela a sua porção inteira.
