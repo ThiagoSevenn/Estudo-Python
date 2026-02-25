ec5f050ac9a2576dd977ee7cab1c6f39a7107a5f
ThiagoSevenn <thiagonevessc@gmail.com>
2026-02-25 11:17:01 -0300

    Desafios 47 - 66

--- desafios/041 - 060/Desafio 053.py
+++ desafios/041 - 060/Desafio 053.py
@@ -1,1 +1,1 @@
- 
+# Crie um programa que leia uma frase qualquer e diga se ela é um políndromo, desconsiderando os espaços
--- desafios/041 - 060/Desafio 054.py
+++ desafios/041 - 060/Desafio 054.py
@@ -1,1 +1,2 @@
- 
+# Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre 
+# quantas pessoas ainda não antingiram a maioridade e quantas já são maiores.
