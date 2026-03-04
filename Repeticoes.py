9b4a0089ad2d75c34a8cc77dca8872899afd7e95
ThiagoSevenn <thiagonevessc@gmail.com>
2026-03-04 11:38:59 -0300

    Desafios 067-070 e apresentação do while para complementar estruturas de repetição

--- Repeticoes.py
+++ Repeticoes.py
@@ -4,8 +4,8 @@
 # Existem algumas formas de desenvolver esses laços, sendo eles utilizando a estrutura "for" e/ou "While",
 # serão apresentados a seguir. 
 
-## Estrutura "for"
-
+## Estrutura "for" conhecida como 'Estrutura de repetição com variável de controle'
+print(f'----{'Estrutura "For"': ^20}----\n')
 ## "Para cada i no intervalo(0,10), faça:"
 print('Laço "For" convencional sem iteração definida.')
 for i in range(0,10):
@@ -18,3 +18,27 @@ print('Laço "For" convencional com iteração definida.')
 for i in range(10,0,-1):
     print('Estou na numeração: ',i)
 
+print('-'*30 + '\n')
+
+
+## Estrutura "while" conhecido como 'Estrutura de repetição com teste lógico'
+print(f'----{'Estrutura "While"': ^20}----\n')
+
+## "Enquanto i for menor que 10 faça:"
+i = 0
+while i < 10:
+    print('Estou na numeração: ',i)
+    i += 1
+
+print()
+    
+## Existe uma preferência em utilizar o While quando não se tem certeza da quantidade de repetições que serão necessárias. Por exemplo:
+numero_desconhecido = 0
+while numero_desconhecido != 10:
+    numero_desconhecido = int(input('Digite um número inteiro: '))
+    print('Esse número não é conhecido.' if numero_desconhecido != 10 else 'Esse número é conhecido, parando o while.')
+
+print('-'*30 + '\n')
+
+
+ 
---
+++ desafios/061 - 080/Desafio 069.py
@@ -0,0 +1,4 @@
+# Crie um programa que leia o nome e o preço de vários produtos. O programa deverá perguntar se o usuário vai continuar. Ao final, mostre:
+# Qual é o total gasto na compra.
+# Quantos produtos custam mais de R$ 1000
+# Qual o nome do produto mais barato.
